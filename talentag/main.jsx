9b26cb247a0bba4ebf4cc409fb2bb70f1296cbb9
Vanessahuang0323 <yutingh88@gmail.com>
2025-05-04 17:06:54 +0800

    Update main.jsx

--- talentag/main.jsx
+++ talentag/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { AppProvider } from "./context/AppContext";
+import { AppProvider } from ".AppContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
